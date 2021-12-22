563918a54f3d84b595f04b8408088046255a0ec8
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2021-12-22 09:49:29 -0500

    Add project files.

---
+++ Day_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//sample input
+int[] input = File.ReadAllLines("C:\\Users\\e378599\\Desktop\\input.txt").Select(input => int.Parse(input)).ToArray();
+
+int numIncreases = 0;
+
+for (int i = 1; i < input.Length; i++)
+{
+    int cur = input[i];
+    int prev = input[i - 1];
+
+    if (cur > prev)
+    {
+        numIncreases++;
+    }
+}
+
+Console.WriteLine(numIncreases);
+
