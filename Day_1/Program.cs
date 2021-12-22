4f64b56333a9fdf229df52798b1166f2fc8be5f3
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2021-12-22 10:51:37 -0500

    day 1 part 2 complete

--- Day_1/Program.cs
+++ Day_1/Program.cs
@@ -1,21 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//part 1 input
+string[] day1Input = File.ReadAllLines("day1_part1_input.txt");
 
-//sample input
-int[] input = File.ReadAllLines("day1_part1_input.txt").Select(input => int.Parse(input)).ToArray();
 
-int numIncreases = 0;
+//part 1 result
+int result = CalculateIncreases(day1Input.Select(input => int.Parse(input)).ToArray());
+Console.WriteLine(result);
 
-for (int i = 1; i < input.Length; i++)
+//part 2 result
+int[] parsedInput = ParsePart2Input(day1Input).Values.ToArray() ;
+int result2 = CalculateIncreases(parsedInput);
+Console.WriteLine(result2);
+
+
+
+
+static int CalculateIncreases(int[] input) 
 {
-    int cur = input[i];
-    int prev = input[i - 1];
+    int numIncreases = 0;
 
-    if (cur > prev)
+    for (int i = 1; i < input.Length; i++)
     {
-        numIncreases++;
+        int cur = input[i];
+        int prev = input[i - 1];
+
+        if (cur > prev)
+        {
+            numIncreases++;
+        }
     }
+    return numIncreases;
 }
 
-Console.WriteLine(numIncreases);
+static Dictionary<int, int> ParsePart2Input(string[] input) 
+{
+    //sample lines
+    //199  A      
+    //200  A B
+    //208  A B C
+    //207  E F   D
+    Dictionary<int, int> output = new();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (!output.ContainsKey(i))
+        {
+            output.Add(i, int.Parse(input[i]));
+
+            if (i == 1)
+            {
+                output[i - 1] += int.Parse(input[i]);
+            } 
+            else if (i > 1) 
+            {
+                output[i - 1] += int.Parse(input[i]);
+                output[i - 2] += int.Parse(input[i]);
+            }
+        }
+    }
+
+    return output;
+}
 
